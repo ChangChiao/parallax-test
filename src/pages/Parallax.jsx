d03c69b8740df2bc19f89089a68ef918f9d352af
joe <joe.chang1014@gmail.com>
2022-10-06 09:15:46 +0800

    feat Parallax test

--- src/pages/Parallax.jsx
+++ src/pages/Parallax.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-
-const Parallax = () => {
-  return <div>111</div>;
+import { useEffect } from "react";
+import Parallax from "parallax-js";
+import bg1 from "../assets/images/bg1.jpg";
+import bg2 from "../assets/images/bg2.jpg";
+import bg3 from "../assets/images/bg3.jpg";
+const ParallaxPage = () => {
+  useEffect(() => {
+    var scene = document.getElementById("scene");
+    var parallaxInstance = new Parallax(scene);
+  }, []);
+  return (
+    <div id="scene">
+      <div data-depth="0.2">
+        <img src={bg1} />
+      </div>
+      <div className="absolute right-0 " data-depth="0.6">
+        <img src={bg2} />
+      </div>
+      <div data-depth="0.8">
+        {/* <img src={bg3} /> */}
+        third layer
+      </div>
+    </div>
+  );
 };
 
-export default Parallax;
+export default ParallaxPage;
