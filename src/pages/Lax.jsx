9686452b5c9ac90795bf5873aa83125cd7c6ed2d
joe <joe.chang1014@gmail.com>
2022-10-06 16:28:10 +0800

    feat lax test

--- src/pages/Lax.jsx
+++ src/pages/Lax.jsx
@@ -1,7 +1,32 @@
-import React from "react";
-
+import { useEffect } from "react";
+import lax from "lax.js";
 function Lax() {
-  return <div>Lax</div>;
+  useEffect(() => {
+    window.onload = function () {
+      lax.init();
+
+      // Add a driver that we use to control our animations
+      lax.addDriver("scrollY", function () {
+        return window.scrollY;
+      });
+
+      // Add animation bindings to elements
+      lax.addElements(".selector", {
+        scrollY: {
+          translateX: [
+            ["elInY", "elCenterY", "elOutY"],
+            [0, "screenWidth/2", "screenWidth"],
+          ],
+        },
+      });
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="selector">Hello</div>
+    </div>
+  );
 }
 
 export default Lax;
