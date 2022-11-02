d3b37c963fbbd006baf870dbadda16d8d6698e0c
joe <joe.chang1014@gmail.com>
2022-11-02 22:15:52 +0800

    git add lax test

--- src/pages/Lax.jsx
+++ src/pages/Lax.jsx
@@ -1,30 +1,41 @@
 import { useEffect } from "react";
 import lax from "lax.js";
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+const list = [
+  { name: "cursor", path: "laxcursor" },
+  { name: "Inertia", path: "laxInertia" },
+];
 function Lax() {
   useEffect(() => {
-    window.onload = function () {
-      lax.init();
-
-      // Add a driver that we use to control our animations
-      lax.addDriver("scrollY", function () {
-        return window.scrollY;
-      });
-
-      // Add animation bindings to elements
-      lax.addElements(".selector", {
-        scrollY: {
-          translateX: [
-            ["elInY", "elCenterY", "elOutY"],
-            [0, "screenWidth/2", "screenWidth"],
-          ],
-        },
-      });
-    };
+    // window.onload = function () {
+    //   lax.init();
+    //   // Add a driver that we use to control our animations
+    //   lax.addDriver("scrollY", function () {
+    //     return window.scrollY;
+    //   });
+    //   // Add animation bindings to elements
+    //   lax.addElements(".selector", {
+    //     scrollY: {
+    //       translateX: [
+    //         ["elInY", "elCenterY", "elOutY"],
+    //         [0, "screenWidth/2", "screenWidth"],
+    //       ],
+    //     },
+    //   });
+    // };
   }, []);
 
   return (
     <div>
-      <div className="selector">Hello</div>
+      <ul className="flex items-center justify-between">
+        {list.map((item) => (
+          <Link key={item.name} to={item.path}>
+            {item.name}
+          </Link>
+        ))}
+      </ul>
+      <Outlet />
     </div>
   );
 }
